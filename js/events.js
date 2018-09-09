1447ef66c7e0bb46c56f0b7e5fa955fc803084d1
Alisa McCormick <muntananuchat@gmail.com>
2018-09-09 01:19:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -7,7 +7,7 @@ function getIt() {
 
 function frameIt() {
   $('img').on('load', function(load) {
-    $('img').addClass('.tasty')
+    $('img').addClass('tasty')
   })
 }
 $(document).ready(function(){
